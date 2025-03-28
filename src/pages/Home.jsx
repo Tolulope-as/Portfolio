c0d4ca6f1ef74a917eb3c1dd21d27a0064daffa3
Tolulope <arowosegbetolu8@gmail.com>
2025-03-28 00:14:48 -1200

    updated version of Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,7 @@ function Home() {
             <section id="home" className="home ">
                 <div className="about-me open-sans animate__animated animate__pulse">
                     <h1>Nice to meet you! I'm <span>Arowosegbe Tolulope.</span></h1>
-                    <p className="occupation">I am a Frontend web developer based in Nigeria,my journey started out of curiousity but the more i learned the more I was hooked and that was what brought me to where i am today, i'm eager to expand my knowledge and take on new challenges.</p>
+                    <p className="occupation">I am a Frontend web developer based in Nigeria,my journey started out of curiousity but now i'm proudly experienced in creating engaging web applications and user interfaces that bring about positive user experience.</p>
                     <a href="#contact">CONTACT ME</a>
                 </div>
                 <div className="profile-container ">
@@ -45,27 +45,27 @@ function Home() {
             <div className="languages-grid poppins ">
                 <div>
                     <p className="language-type">HTML</p>
-                    <p className="experience">1 year of experience</p>
+                    <p className="experience">Experienced</p>
                 </div>
                 <div>
                     <p className="language-type">CSS</p>
-                    <p className="experience">1 year of experience</p>
+                    <p className="experience">Experienced</p>
                 </div>
                 <div>
                     <p className="language-type">JAVASCRIPT</p>
-                    <p className="experience">6 months of experience</p>
+                    <p className="experience">Intermediate</p>
                 </div>
                 <div>
                     <p className="language-type">REACT</p>
-                    <p className="experience">2 months of experience</p>
+                    <p className="experience">Intermediate</p>
                 </div>
                 <div>
                     <p className="language-type">PYTHON</p>
-                    <p className="experience">2 months of experience</p>
+                    <p className="experience">Junior</p>
                 </div>
                 <div>
-                    <p className="language-type">BOOTSTRAP</p>
-                    <p className="experience">1 year of experience</p>
+                    <p className="language-type">DJANGO</p>
+                    <p className="experience">Junior</p>
                 </div>
             </div>
 
@@ -73,59 +73,55 @@ function Home() {
                 <h2 className="dm-sans">Some of the projects I have worked on</h2>
                 <div className="project-container open-sans">
                     <div>
-                        <img className="project-img" src="images/sunnyside.png" width="100%" loading="lazy" alt="Sunnyside Agency Landing Page" />
-                        <p className="project-name">SUNNYSIDE AGENCY LANDING PAGE</p>
-                        <p className="project-language">HTML CSS JS</p>
-                        <a href="https://tolu-sunnyside-landingpage.vercel.app/" className="project-button">VIEW PROJECT</a>
+                        <img className="project-img" src="images/gillion.png" width="100%" loading="lazy" alt="Sunnyside Agency Landing Page" />
+                        <p className="project-name">GILLION BLOG WEBSITE</p>
+                        <p className="project-language">REACT CSS FIREBASE</p>
+                        <a href="https://gillion-blog.vercel.app/" className="project-button">VIEW PROJECT</a>
                     </div>
-                    <div>
-                        <img className="project-img" src="images/calculator.png" width="100%" loading="lazy" alt="Calculator" />
-                        <p className="project-name">CALCULATOR</p>
-                        <p className="project-language">HTML CSS JS</p>
-                        <a href="https://devtolu-calculator.vercel.app/" className="project-button">VIEW PROJECT</a>
-                    </div>
-                    <div>
-                        <img className="project-img" src="images/perfumewebsite.png" width="100%" loading="lazy" alt="Chanel Perfumes Website" />
-                        <p className="project-name">CHANEL PERFUMES WEBSITE</p>
-                        <p className="project-language">HTML CSS JS</p>
-                        <a href="https://chanel-perfumes-tolu.vercel.app/" className="project-button">VIEW PROJECT</a>
-                    </div>
-                    <div>
-                        <img className="project-img" src="images/weathersite.png" width="100%" loading="lazy" alt="Weather Site" />
-                        <p className="project-name">WEATHER SITE</p>
-                        <p className="project-language">HTML CSS JS</p>
-                        <a href="https://tolu-viewweather.vercel.app/" className="project-button">VIEW PROJECT</a>
-                    </div>
-                    <div>
-                        <img className="project-img" src="images/restaurant.png" width="100%" loading="lazy" alt="Restaurant Website" />
-                        <p className="project-name">RESTAURANT WEBSITE</p>
-                        <p className="project-language">HTML CSS JS BOOTSTRAP</p>
-                        <a href="https://tolu-delicieux-restaurant.vercel.app/" className="project-button">VIEW PROJECT</a>
-                    </div>
-                    <div>
-                        <img className="project-img" src="images/budget-tracker.png" width="100%" loading="lazy" alt="Budget tracker" />
-                        <p className="project-name">BUDGET TRACKER</p>
-                        <p className="project-language">HTML CSS JS</p>
-                        <a href="https://tolu-budget-tracker.vercel.app/" className="project-button">VIEW PROJECT</a>
-                    </div>
+
                     <div>
                         <img className="project-img" src="images/book-haven.png" width="100%" loading="lazy" alt="FAQ Accordion" />
                         <p className="project-name">BOOKHAVEN WEBSITE</p>
-                        <p className="project-language">HTML CSS REACT</p>
+                        <p className="project-language">REACT CSS BOOSTRAP</p>
                         <a href="https://book-haven-ruddy.vercel.app/" className="project-button">VIEW PROJECT</a>
                     </div>
+
                     <div>
-                        <img className="project-img" src="images/didi-travels.png" width="100%" loading="lazy" alt="Result Summary Page" />
-                        <p className="project-name">DIDI-TRAVELS-WEBSITE</p>
-                        <p className="project-language">HTML REACT BOOSTRAP</p>
-                        <a href="https://didi-travels-tours.vercel.app/" className="project-button">VIEW PROJECT</a>
+                        <img className="project-img" src="images/perfumewebsite.png" width="100%" loading="lazy" alt="Chanel Perfumes Website" />
+                        <p className="project-name">CHANEL PERFUMES </p>
+                        <p className="project-language">HTML CSS JS</p>
+                        <a href="https://chanel-perfumes-tolu.vercel.app/" className="project-button">VIEW PROJECT</a>
                     </div>
+
                     <div>
                         <img className="project-img" src="images/boomplay.png" width="100%" loading="lazy" alt="Boomplay Website" style={{ border: "1px solid #ffffff4d" }} />
                         <p className="project-name">BOOMPLAY CLONE</p>
                         <p className="project-language">HTML CSS JS</p>
                         <a href="https://tolu-boomplaywebsite.vercel.app/" className="project-button">VIEW PROJECT</a>
                     </div>
+
+                    <div>
+                        <img className="project-img" src="images/didi-travels.png" width="100%" loading="lazy" alt="Result Summary Page" />
+                        <p className="project-name">DIDI-TRAVELS-WEBSITE</p>
+                        <p className="project-language">HTML REACT BOOSTRAP</p>
+                        <a href="https://didi-travels-tours.vercel.app/" className="project-button">VIEW PROJECT</a>
+                    </div>
+                   
+                    <div>
+                        <img className="project-img" src="images/weathersite.png" width="100%" loading="lazy" alt="Weather Site" />
+                        <p className="project-name">WEATHER APP</p>
+                        <p className="project-language">HTML CSS JS</p>
+                        <a href="https://tolu-viewweather.vercel.app/" className="project-button">VIEW PROJECT</a>
+                    </div>
+                    
+                     <div>
+                        <img className="project-img" src="images/budget-tracker.png" width="100%" loading="lazy" alt="Budget tracker" />
+                        <p className="project-name">BUDGET TRACKER</p>
+                        <p className="project-language">HTML CSS JS</p>
+                        <a href="https://tolu-budget-tracker.vercel.app/" className="project-button">VIEW PROJECT</a>
+                    </div> 
+                   
+                   
                     <div>
                         <img className="project-img" src="images/travelsite.png" width="100%" loading="lazy" alt="TravelBotswana Website" />
                         <p className="project-name">TRAVELBOTSWANA WEBSITE</p>
